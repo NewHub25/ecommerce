428499a1fa711bb82d7b2b1a1b530c4b7391d730
Terry <master_correo144@hotmail.com>
2023-07-15 06:41:01 -0500

    ORDER directories and ADD sass files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import './styles/index.css'
+import javascriptLogo from '/img/javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
