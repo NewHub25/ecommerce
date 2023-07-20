287cbe38cb9f379818787371383744da7a59ec86
Terry <master_correo144@hotmail.com>
2023-07-20 10:13:39 -0500

    FIXED menu, color and display: none

---
+++ src/js/toggle-menu.js
@@ -0,0 +1,5 @@
+document
+  .querySelector(".menu__ul")
+  .addEventListener("click", function (e) {
+    this.classList.toggle("active");
+  });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./styles/index.css";
 import "./js/form.js";
+import "./js/toggle-menu.js";
 import { maskImg } from "./js/mask-img.js";
 import javascriptLogo from "/img/javascript.svg";
 import { setupCounter } from "./js/counter.js";
