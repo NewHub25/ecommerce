115239f1827f6db1b9fb101a32e292a9dc90ac56
Terry <master_correo144@hotmail.com>
2023-08-01 13:48:09 -0500

    ADD dialog element

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -1,4 +1,23 @@
-export function showMessage(msg = 'ALGO EXTRAÑO OCURRIÓ') {
-  document.querySelector("dialog h2").innerHTML = msg;
+export function showMessage(
+  h2 = "ALGO EXTRAÑO OCURRIÓ",
+  imgUrl,
+  name,
+  price,
+  description
+) {
+  const categories = ["electro", "jewelry", "her", "him"];
+  // Para que no aparezcan las palabras en inglés de las categorías
+  let contentHTML = `
+  <button onclick="this.parentElement.close()">X</button>
+  <h2>${categories.includes(h2) ? "" : h2}</h2>
+  `;
+  if (imgUrl && name && price && description) {
+    contentHTML += `
+<img src="${imgUrl}" alt="${name}" />
+<h3>${name} <span>${price}$</span></h3>
+<p>${description}</p>
+    `;
+  }
+  document.querySelector("dialog").innerHTML = contentHTML;
   document.querySelector("dialog").showModal();
 }
--- src/js/modifyOwnCar.js
+++ src/js/modifyOwnCar.js
@@ -1,17 +1,23 @@
 //Las funciones deben estar necesariamente dentro del elemento: ".category__ul"
 
+import { showMessage } from "./backdrop";
+import { isUserIn } from "./userIn";
+
 export function clicksInProduct() {
   document.querySelectorAll(".category__ul").forEach((lista) => {
     if (!lista.children.length) return;
     lista.addEventListener("click", (e) => {
       if (e.target.classList.contains("buttonInCar")) {
-        alert("Agregaste un producto con ID: " + e.target.dataset.id);
+        if (!isUserIn()) {
+          location = import.meta.env.BASE_URL + "pages/login.html";
+        } else {
+          alert("Agregaste un producto con ID: " + e.target.dataset.id);
+        }
       }
       if (e.target.classList.contains("buttonView")) {
-        alert(`Estás viendo: ${e.target.dataset.name}`);
+        const { category, url, name, price, description } = e.target.dataset;
+        showMessage(category, url, name, price, description);
       }
     });
   });
-  // location=(import.meta.env.BASE_URL);
 }
-
